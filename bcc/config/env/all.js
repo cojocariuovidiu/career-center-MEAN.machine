7d104dc11aea78dff963243873501c4ac533a72b
donnatabique <dtabique@asu.edu>
2015-08-08 16:37:29 -0700

    routing update

--- bcc/config/env/all.js
+++ bcc/config/env/all.js
@@ -26,8 +26,7 @@ module.exports = {
                 'public/lib/angular-ui-router/release/angular-ui-router.js',
                 'public/lib/angular-ui-utils/ui-utils.js',
                 'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
-                'public/lib/bootstrap/*/*/*.js',
-                'public/lib/jquery/*.js'
+                'public/lib/bootstrap/*/*/*.js'
 
            ],
         },
