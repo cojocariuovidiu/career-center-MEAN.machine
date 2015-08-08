61153a3f6e792217f3ec89c829a4970c428fcc96
donnatabique <dtabique@asu.edu>
2015-08-08 11:28:06 -0700

    ui + content changes
    nabber, home, signup, edit password, edit profile

--- bcc/public/modules/articles/config/articles.client.config.js
+++ bcc/public/modules/articles/config/articles.client.config.js
@@ -2,10 +2,12 @@
 
 // Configuring the Articles module
 angular.module('articles').run(['Menus',
-	function(Menus) {
-		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Articles', 'articles', 'dropdown', '/articles(/create)?');
-		Menus.addSubMenuItem('topbar', 'articles', 'List Articles', 'articles');
-		Menus.addSubMenuItem('topbar', 'articles', 'New Article', 'articles/create');
-	}
+ function (Menus) {
+        // Set top bar menu items
+        // ADDS TO "ARTICLES" TO NAVBAR
+
+        /*		Menus.addMenuItem('topbar', 'Articles', 'articles', 'dropdown', '/articles(/create)?');
+        		Menus.addSubMenuItem('topbar', 'articles', 'List Articles', 'articles');
+        		Menus.addSubMenuItem('topbar', 'articles', 'New Article', 'articles/create');*/
+ }
 ]);
